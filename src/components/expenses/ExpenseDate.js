9ea75eefb62833832dfeb8ff6f5730a1d8ae3f91
UchaChutkerashvili <uchutkerashvili@gmail.com>
2022-04-02 16:21:15 +0400

    first commit

---
+++ src/components/expenses/ExpenseDate.js
@@ -0,0 +1,16 @@
+import './ExpenseDate.css';
+function ExpenseDate(props) {
+  const month = props.date.toLocaleString("en-Us", { month: "long" });
+  const day = props.date.toLocaleString("en-Us", { day: "2-digit" });
+  const year = props.date.getFullYear();
+  
+  return (
+    <div className="expense-date">
+      <div>{month}</div>
+      <div>{day}</div>
+      <div>{year}</div>
+    </div>
+  );
+}
+
+export default ExpenseDate;
